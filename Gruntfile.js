55eaa54614faeef919af9e3e67f1d362cad7e488
Andrew White <aeewhite@gmail.com>
2015-12-22 18:21:41 -0500

    Major rework of gruntfile to act like I want it to Now we can do `grunt package:*platformName*` to build a release for that platform

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,18 +2,30 @@ module.exports = function(grunt) {
 	// Configuration, Tasks and Plugins.
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'), //Import project data from package.json
-		nwjs: {
-			options: {
-				platforms: ['osx64'],
-				buildDir: './build', // Where the build version of my NW.js app is saved
-				buildType: "versioned",
-				version: 'v0.12.3'
-			},
-			src: ['**/*',"!build/**/*","!node_modules/grunt/**/*","!node_modules/grunt-nw-builder/**/*","!cache/**/*"] // Your NW.js app
-		},
+			package: {
+				mac:{
+					options: {
+						platforms: ['osx64'],
+						buildDir: './build', // Where the build version of my NW.js app is saved
+						buildType: "versioned",
+						version: 'v0.12.3'
+					},
+					src: ['**/*',"!build/**/*","!node_modules/grunt/**/*","!node_modules/grunt-nw-builder/**/*","!cache/**/*"] // Your NW.js app
+				},
+				win:{
+					options: {
+						platforms: ['win64'],
+						buildDir: './build', // Where the build version of my NW.js app is saved
+						buildType: "versioned",
+						version: 'v0.12.3'
+					},
+					src: ['**/*',"!build/**/*","!node_modules/grunt/**/*","!node_modules/grunt-nw-builder/**/*","!cache/**/*"] // Your NW.js app
+				}
+			}
 	});
 
 	grunt.loadNpmTasks('grunt-nw-builder');
 	// Register the default tasks.
-	grunt.registerTask('default', ['nwjs']);
+	grunt.task.renameTask('nwjs','package');
+	grunt.registerTask('default',['package']);
 };
